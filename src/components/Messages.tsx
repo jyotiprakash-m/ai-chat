a68cb3fd402f46090b200c34d41d0f853faaf573
jyotiprakash-m <teachmeonline365@gmail.com>
2024-08-17 01:07:35 +0530

    complete code

---
+++ src/components/Messages.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { type Message as TMessage } from "ai/react";
+import Message from "./Message";
+import { MessageSquare } from "lucide-react";
+interface MessagesProps {
+  messages: TMessage[];
+}
+
+const Messages = ({ messages }: MessagesProps) => {
+  return (
+    <div className="flex min-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-scroll">
+      {messages.length ? (
+        messages.map((message, i) => (
+          <Message
+            key={i}
+            content={message.content}
+            isUserMessage={message.role === "user"}
+          />
+        ))
+      ) : (
+        <div className="flex flex-1 flex-col items-center justify-center gap-2">
+          <MessageSquare className="size-8 text-blue-500" />
+          <h3 className="font-semibold text-xl text-white">You are all set!</h3>
+          <p className="text-zinc-500 text-sm">
+            Ask your first question to get started.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Messages;
